b32a987c7ef163b97953b385caf80f11227ef21d
Fedor <fedor.vedernikov@nordigy.ru>
2020-11-05 19:40:12 +0300

    add test with basket

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,12 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    BASKET_LINK = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div h1")
+    BASKET_NAME = (By.CSS_SELECTOR, "#messages strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    BASKET_PRICE = (By.CSS_SELECTOR, ".alertinner p strong")
+
---
+++ pages/product_page.py
@@ -0,0 +1,19 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        link = self.browser.find_element(*ProductPageLocators.BASKET_LINK)
+        link.click()
+
+    def name_is_correct(self):
+        prod_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        bask_name = self.browser.find_element(*ProductPageLocators.BASKET_NAME).text
+        assert prod_name == bask_name, "Uncorrect product"
+
+    def price_is_correct(self):
+        prod_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        bask_price = self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text
+        assert prod_price == bask_price, "Uncorrect price"
